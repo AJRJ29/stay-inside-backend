a1955f8ca2db8e7ff4bdcdafa0446bacdcfd4857
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-07-26 11:07:59 -0700

    Min is cool

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// User Schema
+const CommentSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  eventId: {
+    type: String,
+    required: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+});
+module.exports = Comment = mongoose.model('comments', CommentSchema);
+
---
+++ routes/comments.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+const Comment = require('../models/Comment')
+// Show
+
+router.get('/', (req, res) => {
+  Comment.find()
+    .then(comments => {
+      res.send(comments)
+    })
+    .catch(err => console.error(err))
+})
+// Create
+router.post('/', (req, res) => {
+  // check the body of the request for empty string and remove them from the body
+  console.log(':football:')
+  console.log(req.body)
+  console.log(':football:')
+  // res.send(req.body)
+  Comment.create(req.body)
+    .then(newComment => {
+      res.send(newComment)
+    })
+    .catch(err => console.error(err))
+})
+// Update
+router.put('/:id', (req, res) => {
+  Comment.findOneAndUpdate(
+    {_id: req.params.id},
+    req.body,
+    { new: true }
+  )
+  .then(updatedComment => {
+    res.send(updatedComment)
+  })
+  .catch(err => console.error(err))
+})
+// Delete
+router.delete('/:id', (req, res) => {
+  Comment.findOneAndDelete({_id: req.params.id })
+    .then(deleteComment => {
+      console.log(deleteComment)
+      res.send({message: 'Successful Deletion'})
+    })
+    .catch(err => console.error(err))
+})
+module.exports = router;
+
+
+
+
+
